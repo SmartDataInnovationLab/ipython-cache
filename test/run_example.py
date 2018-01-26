30d21ac21cc5ce67ce70e6904af6f82344977a55
Björn Jürgens <juergens@teco.edu>
2018-01-26 10:49:59 +0100

    prepared setup.py for release
    fixed error "IOError: [Errno 2] No such file or directory: '/tmp/pip-build-OkP4OW/ipython-cache/README.md'
    "

---
+++ test/run_example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors import CellExecutionError
+
+import os
+import shutil
+
+notebook_filename = "example.ipynb"
+
+test_base_dir = "test"
+run_path =  os.path.join(test_base_dir, "temp")
+notebook_filename_out = os.path.join(run_path, "example.out.ipynb")
+
+if os.path.exists(run_path):
+    shutil.rmtree(run_path)
+os.makedirs(run_path)
+
+with open(notebook_filename) as f:
+    nb = nbformat.read(f, as_version=4)
+
+ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+try:
+    out = ep.preprocess(nb, {'metadata': {'path': run_path}})
+except CellExecutionError:
+    out = None
+    msg = 'Error executing the notebook "%s".\n\n' % notebook_filename
+    msg += 'See notebook "%s" for the traceback.' % notebook_filename_out
+    print(msg)
+    raise
+finally:
+    with open(notebook_filename_out, mode='wt') as f:
+        nbformat.write(nb, f)
